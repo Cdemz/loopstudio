982e45b2d3f39bb405e3cf426cf3b2b3bbbcb329
Cdemz <gbengaadepoju75@gmail.com>
2023-03-23 18:05:46 +0100

    build

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ function App() {
        <img src={varman} className="varman" alt="logo" />
        <div className='t3xt'>
          <h1>THE LEADER IN INTERRACTIVE VR</h1>
-         <p></p>
          <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
         </div>
       </div>
